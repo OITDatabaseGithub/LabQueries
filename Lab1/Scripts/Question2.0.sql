5057691cd010704dc0646582d8d46ccdd15ff17c
Jacob Asmuth <jacobasmuth@gmail.com>
2017-04-12 16:55:57 -0700

    Create Question2.0.sql

---
+++ Lab1/Scripts/Question2.0.sql
@@ -0,0 +1,9 @@
+	SELECT
+		uc.description AS airline_name,
+		COUNT(*) AS occurrances
+	FROM on_time_performance AS otp
+		INNER JOIN airline.unique_carriers AS uc ON otp.uniquecarrier = uc.uniquecarriersid
+	WHERE year BETWEEN 2000 AND 2009
+		AND depdelayminutes > 0
+	GROUP BY uc.description
+	ORDER BY occurrances

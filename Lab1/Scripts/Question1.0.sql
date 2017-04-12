f022605aff2e223e1b9189a3347550b03e1e528f
Jacob Asmuth <jacobasmuth@gmail.com>
2017-04-12 16:55:27 -0700

    Create Question1.0.sql

---
+++ Lab1/Scripts/Question1.0.sql
@@ -0,0 +1,9 @@
+select
+	year,
+	month,
+	round( avg( arrdelayminutes )::numeric, 2 ) as avg_arrive_delay
+from airline.on_time_performance
+where year = 2005 OR year = 2006
+group by year, month
+order by avg_arrive_delay desc
+limit 5

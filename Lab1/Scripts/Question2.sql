237172c41bd5b5098fb155c62d2c5b1436ad856d
Jacob Asmuth <jacobasmuth@gmail.com>
2017-04-12 16:59:07 -0700

    Update Question2.sql

--- Lab1/Scripts/Question2.sql
+++ Lab1/Scripts/Question2.sql
@@ -1,7 +1,7 @@
 	SELECT
 		uc.description AS airline_name,
 		COUNT(*) AS occurrances
-	FROM on_time_performance AS otp
+	FROM airline.on_time_performance AS otp
 		INNER JOIN airline.unique_carriers AS uc ON otp.uniquecarrier = uc.uniquecarriersid
 	WHERE year BETWEEN 2000 AND 2009
 		AND depdelayminutes > 0

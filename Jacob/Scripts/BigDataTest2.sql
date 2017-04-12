760be6145326e10bc8c2b5826ed440b11137bba4
Jacob Asmuth <jacobasmuth@gmail.com>
2017-04-12 13:15:06 -0700

    Update BigDataTest2.sql

--- Jacob/Scripts/BigDataTest2.sql
+++ Jacob/Scripts/BigDataTest2.sql
@@ -1,9 +1,3 @@
-select
-		year,
-		month,
-		round( avg( arrdelayminutes )::numeric, 2 ) as avg_arrive_delay
-	from airline.on_time_performance
-	where year = 2005 OR year = 2006
-	group by year, month
-	order by avg_arrive_delay desc
-	limit 5
+select airlineid
+from airline.airline
+where airlineid = 5;

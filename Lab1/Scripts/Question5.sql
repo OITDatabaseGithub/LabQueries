b74de46b6fb66b04946a1158a3f7b83b5d4d35d5
Jacob Asmuth <jacobasmuth@gmail.com>
2017-04-12 17:36:56 -0700

    Update Question5.sql

--- Lab1/Scripts/Question5.sql
+++ Lab1/Scripts/Question5.sql
@@ -1,5 +1,5 @@
 	SELECT
-		uc.description as airline_name
+		uc.description as airline_name,
 		COUNT(*) AS occurrances
 	FROM airline.on_time_performance as otp
 		INNER JOIN airline.unique_carriers AS uc ON otp.uniquecarrier = uc.uniquecarriersid

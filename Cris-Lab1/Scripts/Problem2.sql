4464ca3259a8b7cebc6332c6010f99807b9ea44d
Cristopher Laney <crislaney@users.noreply.github.com>
2017-04-13 18:38:26 -0700

    Update Problem2.sql

--- Cris-Lab1/Scripts/Problem2.sql
+++ Cris-Lab1/Scripts/Problem2.sql
@@ -1,9 +1,3 @@
-select
-airname.description
-, count(*) as occurrences
-from airline.unique_carriers as airname
-JOIN airline.on_time_performance as performance
-ON performance.uniquecarrier = airname.uniquecarriersid
-where year > 1999 and depdelayminutes > 0
-group by airname.description
-order by occurrences desc;
+SELECT count(*) as occurrences
+FROM ailine.on_time_performance as performance
+WHERE performance.arrdelayminutes > 60;

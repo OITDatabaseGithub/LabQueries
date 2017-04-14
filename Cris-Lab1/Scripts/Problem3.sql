e81c842bacd0fc13f9913168c90959e49020ee03
Cristopher Laney <crislaney@users.noreply.github.com>
2017-04-13 18:36:38 -0700

    Create Problem3.sql

---
+++ Cris-Lab1/Scripts/Problem3.sql
@@ -0,0 +1,13 @@
+SELECT
+    LEFT(ap.description, 30) AS dest_airport,
+    MAX(arrdelayminutes) AS max_arrival_delay
+FROM
+    airline.on_time_performance AS otp
+    INNER JOIN airline.airport AS ap ON otp.dest = ap.airportid
+WHERE
+    YEAR >= 1990 AND
+    YEAR <= 1999
+GROUP BY
+    dest_airport
+ORDER BY
+    max_arrival_delay DESC;

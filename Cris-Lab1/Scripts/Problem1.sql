2dac791ef8c00314fef5a3c27c303aa1c0c44cb4
Cristopher Laney <crislaney@users.noreply.github.com>
2017-04-13 18:40:37 -0700

    Create Problem1.sql

---
+++ Cris-Lab1/Scripts/Problem1.sql
@@ -0,0 +1,7 @@
+
+select
+year, month, round(avg(arrdelayminutes)::numeric, 2) as avg_arrive_delay
+from airline.on_time_performance as otp
+where year = 2005 OR year = 2006
+group by year, month
+order by avg_arrive_delay;

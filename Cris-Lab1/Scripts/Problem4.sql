cfb79737f858eec59721e505df75c0d7ee6687fd
Cristopher Laney <crislaney@users.noreply.github.com>
2017-04-15 15:14:20 -0700

    Update Problem4.sql

--- Cris-Lab1/Scripts/Problem4.sql
+++ Cris-Lab1/Scripts/Problem4.sql
@@ -5,4 +5,4 @@ FROM on_time_performance as otp
 INNER JOIN airline.unique_carriers AS uc ON otp.uniquecarrier = uc.uniquecarriersid
 WHERE depdelayminutes > 60
 GROUP BY uc.description
-ORDER BY occurrances
+ORDER BY occurrances;

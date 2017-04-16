2c27160c04ab05e9648c4e074f48f82de08c2e0a
Cristopher Laney <crislaney@users.noreply.github.com>
2017-04-16 12:21:17 -0700

    Update CountCancel.sql

--- Cris-Lab1/Scripts/CountCancel.sql
+++ Cris-Lab1/Scripts/CountCancel.sql
@@ -1,5 +1,4 @@
-SELECT year, month, 
-COUNT(*) AS cancellations
+SELECT year, month, COUNT(*) AS cancellations
 FROM airline.on_time_performance as performance
 WHERE year > 1999 AND year < 2013 AND cancelled = 1
 GROUP BY year, month

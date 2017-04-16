83bd8f3c865a4450ed02e7f01178de00b6f8c041
Cristopher Laney <crislaney@users.noreply.github.com>
2017-04-16 11:59:34 -0700

    Create CountCancel.sql

---
+++ Cris-Lab1/Scripts/CountCancel.sql
@@ -0,0 +1,6 @@
+SELECT year, month, 
+COUNT(*) AS cancellations
+FROM airline.on_time_performance as performance
+WHERE year > 1999 AND year < 2013 AND cancelled = 1
+GROUP BY year, month
+ORDER BY year, month, cancellations;

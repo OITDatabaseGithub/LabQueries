fb064966fee2356a7a3376eade127737432e287e
Cristopher Laney <crislaney@users.noreply.github.com>
2017-04-15 22:09:22 -0700

    Update Problem4.sql

--- Cris-Lab1/Scripts/Problem4.sql
+++ Cris-Lab1/Scripts/Problem4.sql
@@ -1,6 +1,5 @@
 SELECT
-uc.description as airline_name,
-COUNT(*) AS occurrances
+uc.description as airline_name, COUNT(*) AS occurrances
 FROM on_time_performance as otp
 INNER JOIN airline.unique_carriers AS uc ON otp.uniquecarrier = uc.uniquecarriersid
 WHERE depdelayminutes > 60

9ce8fdf5d52e9b6aa4fa60cfec2d67366e9d387a
Cristopher Laney <crislaney@users.noreply.github.com>
2017-04-15 22:09:53 -0700

    Update DelayCrash.sql

--- Cris-Lab1/Scripts/DelayCrash.sql
+++ Cris-Lab1/Scripts/DelayCrash.sql
@@ -1,4 +1,5 @@
-SELECT year, month, day, COUNT(*) AS cancellations
+SELECT year, month, day, 
+COUNT(*) AS cancellations
 FROM airline.on_time_performance as performance
 WHERE  AND year > 1999 AND year < 2013
 GROUP BY year, month, day

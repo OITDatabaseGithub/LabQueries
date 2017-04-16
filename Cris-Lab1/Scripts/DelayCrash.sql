28b8b92c4fa280e850d230accb5e2527fb4abd9b
Cristopher Laney <crislaney@users.noreply.github.com>
2017-04-16 09:45:31 -0700

    Update DelayCrash.sql

--- Cris-Lab1/Scripts/DelayCrash.sql
+++ Cris-Lab1/Scripts/DelayCrash.sql
@@ -1,5 +1,12 @@
-SELECT year, month, day, COUNT(*) AS cancellations
+SELECT year, month, COUNT(*) AS cancellations, time.times_late
 FROM airline.on_time_performance as performance
 WHERE year > 1999 AND year < 2013
-GROUP BY year, month, day
-ORDER BY cancellations, year, month, day;
+JOIN
+  (COUNT(*) AS times_late, year, month
+   FROM airline.on_time_performance as otp
+   JOIN airline.unique_carriers AS uc ON otp.uniquecarrier = uc.uniquecarriersid
+   WHERE depdelayminutes > 60
+   GROUP BY year, month) as time;
+ON performance.month = time.month AND performance.year = time.year
+GROUP BY year, month
+ORDER BY cancellations, year, month;

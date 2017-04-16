9ba5cc2a3e3d635f7e0ff137d12cdb2384bac741
Cristopher Laney <crislaney@users.noreply.github.com>
2017-04-16 10:13:35 -0700

    Update DelayCrash.sql

--- Cris-Lab1/Scripts/DelayCrash.sql
+++ Cris-Lab1/Scripts/DelayCrash.sql
@@ -1,9 +1,9 @@
 SELECT year, month, COUNT(*) AS cancellations, time.times_late
 FROM airline.on_time_performance as performance
-INNER JOIN
+EXISTS
   (SELECT otp.year AS time_year, otp.month AS time_month, COUNT(*) AS times_late
    FROM airline.on_time_performance as otp
-   WHERE depdelayminutes > 60
+   WHERE depdelayminutes > 60 AND time_month = performance.month AND time_year = performance.year
    GROUP BY year, month) as time
 ON performance.month = time.time_month AND performance.year = time.time_year
 WHERE year > 1999 AND year < 2013

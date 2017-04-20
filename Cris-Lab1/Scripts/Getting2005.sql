bf6901bb1927c893353c216027b3bad0fd2545dc
Cristopher Laney <crislaney@users.noreply.github.com>
2017-04-20 10:51:46 -0700

    Update Getting2005.sql

--- Cris-Lab1/Scripts/Getting2005.sql
+++ Cris-Lab1/Scripts/Getting2005.sql
@@ -1,1 +1,7 @@
-SELECT * FROM airline.on_time_performance WHERE year = 2005 AND month = 1;
+SELECT * 
+FROM airline.on_time_performance 
+JOIN airline.airline
+ON airline.airline.airlineid = airline.on_time_performance.airlineid
+JOIN airline.airport
+ON airline.airport.airportid = airline.on_time_performance.airportid
+WHERE year = 2005 AND month = 1;
